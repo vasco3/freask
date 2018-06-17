51ab416bb9fe187fec036fa28bfb414a181b5dd1
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2018-06-17 10:59:04 -0700

    Add tag filter (minor)

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,23 @@ import '../node_modules/react-vis/dist/style.css';
 import RiskChart from '../components/risk-chart';
 import { problemMassager } from '../utils/massager';
 import problems from '../data/problems';
+import tags from '../data/tags';
+
+const TAGS_VALUES = Object.values(tags);
 
 class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterTag: 'none',
+    };
+    this.onFilterChange = this.onFilterChange.bind(this);
+  }
+  onFilterChange(event) {
+    this.setState({ filterTag: event.target.name });
+  }
   render() {
-    const data = problemMassager(problems);
+    const data = problemMassager(problems, this.state.filterTag);
     return (
       <div className="App">
         <h1>Freask</h1>
@@ -17,6 +30,11 @@ class Index extends Component {
           moves ahead.
         </p>
         <RiskChart data={data} />
+        {['none', ...TAGS_VALUES].map(tag => (
+          <button name={tag} key={tag} onClick={this.onFilterChange}>
+            {tag}
+          </button>
+        ))}
       </div>
     );
   }
--- utils/massager.js
+++ utils/massager.js
@@ -1,5 +1,10 @@
-export function problemMassager(problems) {
-  return problems.map(({ label, probability, impact }) => ({
+export function problemMassager(problems, filterTag) {
+  const filteredProblems =
+    filterTag === 'none'
+      ? problems
+      : problems.filter(problem => problem.tags.includes(filterTag));
+
+  return filteredProblems.map(({ label, probability, impact }) => ({
     x: impact,
     y: probability,
     label,
